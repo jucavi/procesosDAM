8f88d6c1fb7351f7fd27187cd3e6fbc02b3f8212
Jucavi <jcarvil@gmail.com>
2023-10-20 18:57:01 +0200

    Add ejercicio 4

---
+++ Ejercicio4/src/ejercicio4/Ejercicio4.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+/**
+ *
+ * @author jcarv
+ */
+public class Ejercicio4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String strPath = getInput("Introduzca la ruta donde desea crear la nueva carpeta: ");
+        Path parentPath = absPath(strPath);
+
+        if (Files.exists(parentPath) && Files.isDirectory(parentPath)) {
+            listDir(parentPath);
+
+        } else {
+            System.out.println("La ruta no existe o no es un directorio: " + parentPath.toString());
+        }
+    }
+
+    public static String getInput(String msg) {
+        Scanner sc = new Scanner(System.in, "ISO-8859-1");
+
+        System.out.print(msg);
+        String in = sc.nextLine();
+
+        if (in.endsWith(":")) {
+            in = in.concat("\\");
+        }
+        return in;
+    }
+
+    public static Path absPath(String path) {
+        Path absPath = Paths.get(path);
+
+        if (!absPath.isAbsolute()) {
+            return Paths.get(System.getProperty("user.dir"), absPath.toString());
+        }
+
+        return absPath;
+    }
+
+    public static void listDir(Path path) {
+
+        String osName = System.getProperty("os.name");
+        String[] listCmd;
+
+        if (osName.toUpperCase().contains("WIN")) {
+            listCmd = new String[]{"cmd.exe", "/c", "dir", path.toString()};
+        } else {
+            listCmd = new String[]{"bash", "-c", "ls", "-la", path.toString()};
+        }
+
+        if (listCmd != null) {
+
+            try {
+                ProcessBuilder ps = new ProcessBuilder(listCmd);
+                Process proc = ps.start();
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+                String line;
+                while ((line = br.readLine()) != null) {
+                    System.out.println(line);
+                }
+                br.close();
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+}
